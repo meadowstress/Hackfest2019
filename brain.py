7e1ef2af1360411706d20169e13bc89675c2f2d8
davhor <david.hornfeck@zenuity.com>
2019-12-05 16:56:08 +0100

    Refactor receiver to class and add hardware engage functionality

--- brain.py
+++ brain.py
@@ -1,20 +1,25 @@
 from slack_alarm_bot import SlackAlarmBot
 import relay_communicator
+<<<<<<< HEAD
 import receiver
+=======
+>>>>>>> Refactor receiver to class and add hardware engage functionality
 import time
+import receiver
 
 
 def run():
     print("Hi, I am the brain")
 
-    #commented out below as it is blocking. Need to run asynch.
+    # commented out below as it is blocking. Need to run asynch.
     #alarm_bot = SlackAlarmBot()
 
     count = 0
     max_count = 10
     relay = relay_communicator.RelayCommunicator()
+    received_signal = receiver.Receiver()
     while count < max_count:
-        activation_request = receiver.check_input_from_keyboard()
+        activation_request = received_signal.check_input_from_keyboard()
         print("Activation request = ", activation_request)
         #activation_request = arbitrate(activation_requests)
         relay.activate(activation_request)
--- receiver.py
+++ receiver.py
@@ -1,11 +1,33 @@
-def check_input_dummy():
-    print("Check input, return true")
-    return True
+import RPi.GPIO as GPIO
 
-def check_input_from_keyboard():
-    activation_keys = ["yes", "y", "alarm", "911", "cake","fika", "lunch", "lellky"]
-    print("Check input from keyboard")
-    activation_request_string = input("Do you want to activate? ")
-    print(activation_request_string)
-    activation_request = activation_request_string in activation_keys
-    return activation_request
+
+class Receiver:
+
+    def __init__(self):
+        self.system_engaged = False
+        self.PIN_ENGAGED = 40
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.PIN_ENGAGED, GPIO.IN)
+
+    def __del__(self):
+        print("Delete Receiver")
+        GPIO.cleanup()
+
+    def ceck_input_dummy(self):
+        print("Check input, return true")
+        return True
+
+    def check_input_from_keyboard(self):
+        activation_keys = ["yes", "y", "alarm",
+                           "911", "cake", "fika", "lunch", "lellky"]
+        print("Check input from keyboard")
+        activation_request_string = input("Do you want to activate? ")
+        print(activation_request_string)
+        activation_request = activation_request_string in activation_keys
+        return activation_request
+
+    def check_engage_switch(self):
+        if(GPIO.input(self.PIN_ENGAGED) == GPIO.LOW)
+        return True
+        else
+        return False
