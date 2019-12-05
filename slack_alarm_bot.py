b46c468ad6c3e23619368e1b2b6c0724250f254a
Mikael Hammar <mikael.hammar@zenuity.com>
2019-12-05 17:14:38 +0100

    adding proof of concept for asynchronous message parsing.

--- slack_alarm_bot.py
+++ slack_alarm_bot.py
@@ -1,21 +1,22 @@
 import os
 import slack
+import time
 
+import asyncio
+from multiprocessing import Pool
 
 class SlackAlarmBot:
 
     alarm_on = False;
 
     def __init__(self):
-        slack_token = "xoxb-225841683220-853263508177-bDaFlG3HW4EhW9ddELXaI2oy"
-        self.rtm_client = slack.RTMClient(token=slack_token)
-        self.rtm_client.on(event='message', callback = lambda **kwargs : self.say_hello(**kwargs))
-        self.rtm_client.start()
+        slack_token = "xoxb-225841683220-853263508177-IvnW1mvwqISHAIvgxBokO35x"
+        self.rtm_client = slack.RTMClient(token=slack_token, run_async=True)
+        self.rtm_client.on(event='message', callback =  self.say_hello)
 
     def say_hello(self, **payload):
         data = payload['data']
         web_client = payload['web_client']
-        rtm_client = payload['rtm_client']
         if 'Hello' in data.get('text', []):
             channel_id = data['channel']
             thread_ts = data['ts']
@@ -32,5 +33,18 @@ class SlackAlarmBot:
     def check_input_dummy(self):
         return self.alarm_on;
 
+async def foo():
+    while True:
+        print("say hi")
+        await asyncio.sleep(5)
+
 if __name__ == "__main__":
+
     alarm_bot = SlackAlarmBot()
+    loop = asyncio.get_event_loop()
+    loop.create_task(alarm_bot.rtm_client.start())
+    loop.create_task(foo())
+    loop.run_forever()
+    loop.close()
+
+
