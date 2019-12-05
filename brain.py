8f05d3627ae16f3deaf297618c161d1b0610b84d
davhor <david.hornfeck@zenuity.com>
2019-12-05 18:20:21 +0100

    Revert refacoring of receiver and brain

--- brain.py
+++ brain.py
@@ -4,19 +4,20 @@ import relay_communicator
 import time
 import receiver
 
+
 async def do_work():
     count = 0
     max_count = 10
     relay = relay_communicator.RelayCommunicator()
-    received_signal = receiver.Receiver()
     while count < max_count:
-        activation_request = received_signal.check_input_from_keyboard()
+        activation_request = receiver.check_input_from_keyboard()
         print("Activation request = ", activation_request)
         #activation_request = arbitrate(activation_requests)
         relay.activate(activation_request)
         count = count + 1
         print("sleep")
         time.sleep(1)
+
 
 def run():
     print("Hi, I am the brain")
--- receiver.py
+++ receiver.py
@@ -1,30 +1,13 @@
-import RPi.GPIO as GPIO
+def check_input_dummy():
+    print("Check input, return true")
+    return True
 
 
-class Receiver:
-
-    def __init__(self):
-        self.system_engaged = False
-        self.PIN_ENGAGED = 40
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.PIN_ENGAGED, GPIO.IN)
-
-    def __del__(self):
-        print("Delete Receiver")
-        GPIO.cleanup()
-
-    def check_input_dummy(self):
-        print("Check input, return true")
-        return True
-
-    def check_input_from_keyboard(self):
-        activation_keys = ["yes", "y", "alarm",
-                           "911", "cake", "fika", "lunch", "lellky"]
-        print("Check input from keyboard")
-        activation_request_string = input("Do you want to activate? ")
-        print(activation_request_string)
-        activation_request = activation_request_string in activation_keys
-        return activation_request
-
-    def check_engage_switch(self):
-        return(GPIO.input(self.PIN_ENGAGED) == GPIO.LOW)
+def check_input_from_keyboard():
+    activation_keys = ["yes", "y", "alarm",
+                       "911", "cake", "fika", "lunch", "lellky"]
+    print("Check input from keyboard")
+    activation_request_string = input("Do you want to activate? ")
+    print(activation_request_string)
+    activation_request = activation_request_string in activation_keys
+    return activation_request
