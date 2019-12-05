cf5c3e8032d862fb61847bd72fc5e42b688c52a2
tobrie <tobias.riemenschneider@zenuity.com>
2019-12-05 14:03:48 +0100

    integrate hardware_test in relay_communicator

--- hardware_test.py
+++ hardware_test.py
@@ -1,26 +1,26 @@
 #https://medium.com/@isma3il/install-python-3-6-or-3-7-and-pip-on-raspberry-pi-85e657aadb1e
+def run():
+    print("Hallo Welt!")
 
-print("Hallo Welt!")
+    import RPi.GPIO as GPIO
+    import time
 
-import RPi.GPIO as GPIO
-import time
+    OUTPUT_PIN = 3 # GPIO2
 
-OUTPUT_PIN = 3 # GPIO2
+    # to use Raspberry Pi board pin numbers
+    GPIO.setmode(GPIO.BOARD)
+    # Specify OUTPUT_PIN as output
+    GPIO.setup(OUTPUT_PIN, GPIO.OUT)
 
-# to use Raspberry Pi board pin numbers
-GPIO.setmode(GPIO.BOARD)
-# Specify OUTPUT_PIN as output
-GPIO.setup(OUTPUT_PIN, GPIO.OUT)
-
-# Initialize output pin with HIGH (deactive)
-GPIO.output(OUTPUT_PIN, GPIO.HIGH)
-
-# Let it blink
-for i in range(5):
-    GPIO.output(OUTPUT_PIN, GPIO.LOW)
-    time.sleep(5.0)
+    # Initialize output pin with HIGH (deactive)
     GPIO.output(OUTPUT_PIN, GPIO.HIGH)
-    time.sleep(1.0)
 
-GPIO.cleanup()
+    # Let it blink
+    for i in range(5):
+        GPIO.output(OUTPUT_PIN, GPIO.LOW)
+        time.sleep(5.0)
+        GPIO.output(OUTPUT_PIN, GPIO.HIGH)
+        time.sleep(1.0)
+
+    GPIO.cleanup()
 
--- relay_communicator.py
+++ relay_communicator.py
@@ -1,2 +1,5 @@
+import hardware_test
+
 def switch_on_relay():
     print("Turn on the relay!")
+    hardware_test.run()
