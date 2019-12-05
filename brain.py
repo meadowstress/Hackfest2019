779037798445bbc913182af58766f50961fa1d9c
davhor <david.hornfeck@zenuity.com>
2019-12-05 14:23:56 +0100

    Add exit handler for cleanup brain

--- brain.py
+++ brain.py
@@ -1,13 +1,23 @@
 import receiver
 import relay_communicator
+import atexit
+import RPi.GPIO as GPIO
+
+
+def cleanup():
+    GPIO.cleanup()
+
 
 def run():
     print("Hi, I am the brain")
     count = 0
-    max_count = 10;
+    max_count = 10
     while count < max_count:
         relay_communicator.activate_relay(receiver.check_input_dummy())
         count = count + 1
+
+    atexit.register(cleanup)
+
 
 if __name__ == '__main__':
     exit(run())
