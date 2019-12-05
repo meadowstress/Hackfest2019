d6d4846253241b7e3daffd27522bdc1e91aa44fa
Tobias Riemenschneider <tobias.riemenschneider@zenuity.com>
2019-12-05 18:23:25 +0100

    fix test - receiver changed back

--- test_input_from_keyboard.py
+++ test_input_from_keyboard.py
@@ -1,7 +1,6 @@
 import receiver
 
-rc = receiver.Receiver()
-activation_request = rc.check_input_from_keyboard()
+activation_request = receiver.check_input_from_keyboard()
 if activation_request:
     print("yes")
 else:
