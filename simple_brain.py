d566b58484014b3726a81f061b3ed2544c430d7a
Tobias Riemenschneider <tobias.riemenschneider@zenuity.com>
2019-12-05 18:26:26 +0100

    add simple brain that takes user input from keyboard

---
+++ simple_brain.py
@@ -0,0 +1,21 @@
+import relay_communicator
+import time
+import receiver
+
+
+def run():
+    print("Hi, I am the stupid brain")
+    count = 0
+    max_count = 10
+    relay = relay_communicator.RelayCommunicator()
+    while count < max_count:
+        activation_request = receiver.check_input_from_keyboard()
+        print("Activation request = ", activation_request)
+        relay.activate(activation_request)
+        count = count + 1
+        print("sleep")
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    exit(run())
